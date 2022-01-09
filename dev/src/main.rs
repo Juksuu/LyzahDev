f008a47541425b99afff9ae68201ea0ef341b8e7
Frans Paasonen <franspaasonen@gmail.com>
2022-01-09 20:11:00 +0200

    feat: Create a basic application and run it

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,19 +1,6 @@
-use lyzah::{core, sdl};
+use lyzah::Application;
 
 fn main() {
-    let mut app = core::App::new();
-    app.add_ticker(1);
-
-    app.start_ticker(Box::new(|| {
-        println!("tick");
-    }));
-
-    let game_window = sdl::context::GameWindowContext::new().init();
-
-    println!(
-        "Using SDL renderer: {}",
-        game_window.get_info().unwrap().name
-    );
-
-    game_window.run_render_loop();
+    let game = Application::new();
+    game.run();
 }
