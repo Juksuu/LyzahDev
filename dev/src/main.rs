3b39ad2a300487a827ac756dfee01ef310c0c999
Frans Paasonen <franspaasonen@gmail.com>
2022-01-17 01:37:18 +0200

    feat: Create two sprites with different pos and rot

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,13 +1,19 @@
 use lyzah::{Application, Sprite};
-use std::path::Path;
+use std::{f32::consts::PI, path::Path};
 
 fn main() {
     let mut game = Application::new();
 
     let asset_path = Path::new(env!("OUT_DIR")).join("res");
-    let sprite = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+    let mut sprite = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+    sprite.set_position(100.0, -200.0);
+    sprite.set_rotation(-PI / 2.0);
+
+    let mut sprite2 = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+    sprite2.set_position(-300.0, -200.0);
 
     game.stage.children.push(sprite);
+    game.stage.children.push(sprite2);
 
     game.run();
 }
