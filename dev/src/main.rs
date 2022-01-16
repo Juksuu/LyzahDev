000c9a05e5520d7bb01c0f2676f8eea549607449
Frans Paasonen <franspaasonen@gmail.com>
2022-01-16 21:18:00 +0200

    feat: Add sprite usage

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,6 +1,13 @@
-use lyzah::Application;
+use lyzah::{Application, Sprite};
+use std::path::Path;
 
 fn main() {
-    let game = Application::new();
+    let mut game = Application::new();
+
+    let asset_path = Path::new(env!("OUT_DIR")).join("res");
+    let sprite = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+
+    game.stage.children.push(sprite);
+
     game.run();
 }
