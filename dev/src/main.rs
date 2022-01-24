5f017df2acfcf7cc07dc86f895d9896ca0625b27
Frans Paasonen <franspaasonen@gmail.com>
2022-01-24 18:18:11 +0200

    feat: Add camera

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -6,14 +6,14 @@ fn main() {
 
     let asset_path = Path::new(env!("OUT_DIR")).join("res");
     let mut sprite = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
-    sprite.set_position(100.0, -200.0);
+    // sprite.set_position(100.0, -200.0);
     sprite.set_rotation(-PI / 2.0);
 
     let mut sprite2 = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
-    sprite2.set_position(-300.0, -200.0);
+    sprite2.set_position(-300.0, -0.0);
 
-    game.stage.children.push(sprite);
-    game.stage.children.push(sprite2);
+    game.renderables.push(sprite);
+    game.renderables.push(sprite2);
 
     game.run();
 }
