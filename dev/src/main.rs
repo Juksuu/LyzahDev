e7df8ece5086883af80f1c82bcc71ac1c398c1bb
Frans Paasonen <franspaasonen@gmail.com>
2022-02-13 00:02:47 +0200

    feat(resources): Add resource loading

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -5,13 +5,16 @@ fn main() {
     let mut game = Application::new();
 
     let asset_path = Path::new(env!("OUT_DIR")).join("res");
-    let mut sprite = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+    let images = vec![asset_path.join("happy-tree.png").to_path_buf()];
+    game.resources.load_images(images);
+
+    let mut sprite = Sprite::new(game.resources.get(&"happy-tree.png".to_string()));
     sprite.set_anchor(0.5, 0.5);
     sprite.set_scale(0.5, 0.5);
     sprite.set_position(100.0, -100.0);
     sprite.set_rotation(-PI / 4.0);
 
-    let mut sprite2 = Sprite::new(asset_path.join("happy-tree.png").to_path_buf());
+    let mut sprite2 = Sprite::new(game.resources.get(&"happy-tree.png".to_string()));
     sprite2.set_position(-300.0, -0.0);
 
     game.renderables.push(sprite);
