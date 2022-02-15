dc01b1815b2e52b80dc182e664466c1320fcac84
Frans Paasonen <franspaasonen@gmail.com>
2022-02-15 22:40:25 +0200

    feat(input): Add usage of mouse input

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -22,21 +22,24 @@ fn move_player(
     #[resource] time: &Time,
     #[resource] input: &Input,
 ) {
-    let old_pos = sprite.position;
-    let move_amount = 200.0 * time.delta_time.as_secs_f32();
-    let mut pos_change = (old_pos.0, 0.0);
+    if input.is_mouse_inside_window {
+        sprite.set_position(input.mouse_pos.x, input.mouse_pos.y);
+    } else {
+        let old_pos = sprite.position;
+        let move_amount = 200.0 * time.delta_time.as_secs_f32();
+        let mut pos_change = (0.0, 0.0);
 
-    for key in &input.pressed_keys {
-        match key {
-            VirtualKeyCode::A => new_pos.0 -= move_amount,
-            VirtualKeyCode::S => new_pos.1 -= move_amount,
-            VirtualKeyCode::W => new_pos.1 += move_amount,
-            VirtualKeyCode::D => new_pos.0 += move_amount,
-            _ => (),
+        for key in &input.pressed_keys {
+            match key {
+                VirtualKeyCode::A => pos_change.0 -= move_amount,
+                VirtualKeyCode::S => pos_change.1 -= move_amount,
+                VirtualKeyCode::W => pos_change.1 += move_amount,
+                VirtualKeyCode::D => pos_change.0 += move_amount,
+                _ => (),
+            }
         }
+        sprite.set_position(old_pos.x + pos_change.0, old_pos.y + pos_change.1);
     }
-
-    sprite.set_position(old_pos.x + pos_change.0, old_pos.y + pos_change.1);
 }
 
 fn main() {
